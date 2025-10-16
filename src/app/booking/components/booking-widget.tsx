4a87b8a365b38f522a7197c8342bc72d7de55043
Maksim Stolyar <www.maxom0796@gmail.com>
2025-10-16 13:56:04 +0300

    rewrite architecture

--- src/app/booking/components/booking-widget.tsx
+++ src/app/booking/components/booking-widget.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, {useState} from 'react';
+import React from 'react';
 import {Button} from "@/shared/button";
 import { DatePicker } from './date-picker';
 import { TimePicker } from './time-picker';
